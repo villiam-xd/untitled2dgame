021fd4f5137e9e6214843feb24fce7588c58d3c1
villiamxd <villiam.strandh45@gmail.com>
2025-02-10 18:33:17 +0100

    Reworked movement | Added Stickman running anim
    Movement has been reworked using input axis. Added and applied running
    animation that adjusts animation speed with running velocity.

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -6,58 +6,91 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public Collider2D collider;
-    public Rigidbody2D body;
+    public float acceleration;
+    public float maxSpeed;
+    private Vector2 move;
+    
+    //Player object components
+    Collider2D collider;
+    Rigidbody2D body;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        collider = GetComponent<Collider2D>();
+        body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Mathf.Abs(body.velocity.x) < maxSpeed && collider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            body.AddForce(move * acceleration * Time.deltaTime);
+        }
+    }
+
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        //Determines player speed to play running animation
+        animator.SetFloat("Speed", Mathf.Abs(body.velocity.x));
+        
+        //Flips sprite depending on running direction
+        if (body.velocity.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (body.velocity.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        
+        move = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             if (collider.IsTouchingLayers(LayerMask.GetMask("Ground")))
             {
-                //Debug.Log(body.velocity.y);
-                if(body.velocity.y == 0)
-                {
-                    body.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
-
-                }
-                else
-                {
-                    Debug.Log("LMAO");
-                }
-                //Debug.Log("Is Touching Ground");
-                
+                body.AddForce(Vector2.up * 20, ForceMode2D.Impulse);
             }
-            //Debug.Log("W key pressed");
+        }
+    }
 
-        }
-        //if (Input.GetKey(KeyCode.S))
-        //{
-        //    Debug.Log("S key pressed");
-        //    this.gameObject.transform.Translate(Vector3.down * Time.deltaTime * 5);
-        //}
-        if (Input.GetKey(KeyCode.A))
+    void OldMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            //Debug.Log("A key pressed");
-            if(body.velocity.x < 0.1)
+            if (Input.GetKey(KeyCode.W))
             {
-                body.AddForce(Vector2.left);
+                if (collider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+                {
+                    if(body.velocity.y == 0 || true)
+                    {
+                        body.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+
+                    }
+                }
             }
-            
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            //Debug.Log("D key pressed");
-            if (body.velocity.x > -0.1)
+            if (Input.GetKey(KeyCode.A))
             {
-                body.AddForce(Vector2.right);
+                if (body.velocity.x > 0)
+                {
+                    body.velocity = new Vector2(0, body.velocity.y);
+                }
+                Debug.Log("A key pressed");
+                body.AddForce(Vector2.left * 20, ForceMode2D.Impulse);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                if (body.velocity.x < 0)
+                {
+                    body.velocity = new Vector2(0, body.velocity.y);
+                }
+                Debug.Log("D key pressed");
+                body.AddForce(Vector2.right * 20, ForceMode2D.Impulse);
             }
         }
     }
